aa04173061319a37d7ae2ce59c811cfb4b3533c7
zow1e <zowieong.zo@gmail.com>
2022-08-30 15:29:29 +0800

    rough sketch

--- main.py
+++ main.py
@@ -7,3 +7,12 @@ from werkzeug.utils import secure_filename
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'I have a dream'
 app.config['UPLOAD_FOLDER'] = 'static/images'
+
+
+@app.route('/')
+def home():
+   return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
