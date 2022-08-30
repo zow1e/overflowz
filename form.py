b45ea525838ff73c8037e5e3612ab0768da5578b
zow1e <zowieong.zo@gmail.com>
2022-08-30 16:10:01 +0800

    create

--- form.py
+++ form.py
@@ -1,11 +1,9 @@
 from logging import PlaceHolder
 from wtforms import Form, StringField, SelectField, TextAreaField, PasswordField, validators, DateField, TimeField, FileField, FieldList, FormField, RadioField, IntegerField, SubmitField
 from wtforms.fields import EmailField, DateField
-from flask_wtf import FlaskForm
 
 class carbonForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=150), validators.DataRequired()])
-    email = EmailField('Email', [validators.Email(), validators.DataRequired()])
     birthdate = DateField('Date of Birth', [validators.length(max=8), validators.Optional()])
     password = PasswordField('Password', [validators.Length(min=8, max=20), validators.DataRequired()])
     comfirmpw = PasswordField('Confirm Password', [validators.Length(min=8, max=20), validators.DataRequired()])
