054c7b33f4e888ebb1fad0b47353e6caf7886242
zow1e <zowieong.zo@gmail.com>
2022-09-01 17:29:54 +0800

    session

--- user.py
+++ user.py
@@ -1,7 +1,10 @@
 
 class User:
 
+    form = 0
+
     def __init__(self, email):
+
 
         self.__email = email
         self.__dates = []
@@ -10,8 +13,9 @@ class User:
         self.__food = []
         self.__others = []
         self.__total = []
+        self.__formlist = []
         
-
+        
     def get_email(self):
         return self.__email
     def set_email(self, email):
@@ -26,23 +30,43 @@ class User:
         return self.__utilities
     def set_utilities(self, utilities):
         self.__utilities.append(utilities)
+    def delete_utilities(self, no):
+        self.__utilities.pop(no)
     
     def get_transport(self):
         return self.__transport
     def set_transport(self, transport):
         self.__transport.append(transport)
+    def delete__transport(self, no):
+        self.__transport.pop(no)
     
     def get_food(self):
         return self.__food
     def set_food(self, food):
         self.__food.append(food)
+    def delete__food(self, no):
+        self.__food.pop(no)
     
     def get_others(self):
         return self.__others
     def set_others(self, others):
         self.__others.append(others)
+    def delete__others(self, no):
+        self.__others.pop(no)
 
     def get_total(self):
         return self.__total
     def set_total(self, total):
         self.__total.append(total)
+    def delete__total(self, no):
+        self.__total.pop(no)
+
+    def get_formid(self):
+        return self.__formlist
+
+    def set_formid(self):
+        User.form += 1
+        self.__formlist.append(User.form)
+
+    def delete_formid(self, no):
+        self.__formlist.pop(no)
